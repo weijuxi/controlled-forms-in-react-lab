86e64908cad81b1e0818e8ac23621748fe30ccc5
weijuxi <2568706310@qq.com>
2024-09-25 13:14:29 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // src/App.jsx
+import { useState } from 'react';
+
+import './App.css';
+import Bookshelf from './Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
